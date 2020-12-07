2f19958b9bef3b57c423ce3fe8fc35b135f12d74
Maximilian Fricke <mfricke2808@gmail.com>
2020-12-07 20:35:52 +0100

    avoid string allocations

--- src/day07/mod.rs
+++ src/day07/mod.rs
@@ -5,13 +5,9 @@ enum Error {
     InvalidInput,
 }
 
-type Id = i32;
-type IdSet = HashMap<String, Id>;
-type RuleSet = HashMap<Id, Vec<(i32, Id)>>;
+type RuleSet<'a> = HashMap<&'a str, Vec<(i32, &'a str)>>;
 
-fn parse_rules(s: &str) -> crate::Result<(IdSet, RuleSet)> {
-    let mut id_cnt = -1;
-    let mut ids = HashMap::new();
+fn parse_rules(s: &str) -> crate::Result<RuleSet> {
     let rules = s
         .lines()
         .map(|l| -> crate::Result<_> {
@@ -26,45 +22,36 @@ fn parse_rules(s: &str) -> crate::Result<(IdSet, RuleSet)> {
                 ts[1]
                     .split(", ")
                     .map(|e| -> crate::Result<_> {
-                        let mut ws = e.split(' ');
-                        let cnt: i32 = ws
-                            .next()
-                            .and_then(|w| w.parse().ok())
-                            .ok_or_else(|| crate::Error::boxed(Error::InvalidInput))?;
+                        let e = e
+                            .trim_matches('.')
+                            .trim_end_matches(" bag")
+                            .trim_end_matches(" bags");
 
-                        let clr: Vec<_> = ws.take_while(|w| !w.starts_with("bag")).collect();
-                        let clr = *ids.entry(clr.join(" ")).or_insert_with(|| {
-                            id_cnt += 1;
-                            id_cnt
-                        });
+                        let cnt = e.trim_matches(|c: char| !c.is_numeric()).parse::<i32>()?;
+                        let clr = e.trim_matches(|c: char| c.is_numeric() || c == ' ');
 
                         Ok((cnt, clr))
                     })
                     .collect::<Result<_, _>>()?
             };
 
-            Ok((
-                *ids.entry(ts[0].to_string()).or_insert_with(|| {
-                    id_cnt += 1;
-                    id_cnt
-                }),
-                es,
-            ))
+            Ok((ts[0], es))
         })
         .collect::<Result<_, _>>()?;
-    Ok((ids, rules))
+
+    Ok(rules)
 }
 
-fn find_outer_layers(clr: i32, rules: &RuleSet, layers: &mut HashSet<Id>) {
+fn find_outer_layers<'a>(clr: &str, rules: &'a RuleSet, layers: &mut HashSet<&'a str>) {
     for (k, v) in rules.iter() {
         if v.iter().any(|(_, c)| *c == clr) {
-            layers.insert(*k);
+            layers.insert(k);
             find_outer_layers(*k, rules, layers);
         }
     }
 }
 
-fn count_contained_bags(clr: i32, rules: &RuleSet) -> crate::Result<i32> {
+fn count_contained_bags(clr: &str, rules: &RuleSet) -> crate::Result<i32> {
     let mut cnt = 0;
     for (ccnt, cclr) in rules
         .get(&clr)
@@ -76,24 +63,13 @@ fn count_contained_bags(clr: i32, rules: &RuleSet) -> crate::Result<i32> {
 }
 
 pub fn part1(input: &str) -> crate::Result<i32> {
-    let (ids, rules) = parse_rules(input)?;
-
+    let rules = parse_rules(input)?;
     let mut layers = HashSet::new();
-    find_outer_layers(
-        *ids.get("shiny gold")
-            .ok_or_else(|| crate::Error::boxed(Error::InvalidInput))?,
-        &rules,
-        &mut layers,
-    );
-
+    find_outer_layers("shiny gold", &rules, &mut layers);
     Ok(layers.len() as i32)
 }
 
 pub fn part2(input: &str) -> crate::Result<i32> {
-    let (ids, rules) = parse_rules(input)?;
-    Ok(count_contained_bags(
-        *ids.get("shiny gold")
-            .ok_or_else(|| crate::Error::boxed(Error::InvalidInput))?,
-        &rules,
-    )?)
+    let rules = parse_rules(input)?;
+    Ok(count_contained_bags("shiny gold", &rules)?)
 }
